f8f24ae562b3000f5488cf4bd17583863d0eba5a
Tongguang Watanabe Xia <ghbq674521@163.com>
2021-05-02 16:24:50 +0800

    add user system

--- flopfarm/flopfarm_admin/urls.py
+++ flopfarm/flopfarm_admin/urls.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-04-29 21:33:43
-LastEditTime: 2021-05-01 23:33:31
+LastEditTime: 2021-05-02 15:57:41
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /FlopFarmAdminLTE/flopfarm/flopfarm_admin/urls.py
@@ -11,7 +11,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('dashboard/', views.dashboard, name = 'dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('market/', views.market, name = 'market')
 ]
